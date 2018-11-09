6c48ce41e927a6906f8a95ff59ccb0e93b306d4b
Wansiliang <1317489420@qq.com>
2018-11-10 00:23:15 +0800

    Update list-item.jsx

--- client/views/topic-list/list-item.jsx
+++ client/views/topic-list/list-item.jsx
@@ -16,7 +16,7 @@ import {Topic} from '../../store/topics-store'
 })
 @observer
 export default class ListItems extends React.Component {
-  // 这safas一sdgs步7779999是sdgsdgsg重点sdg77888
+  // 这safas一sdgs步7779999是sdgsdgsg重点sdg77888gdgdgdg
   static contextTypes = {
     router: PropTypes.object.isRequired
   };
